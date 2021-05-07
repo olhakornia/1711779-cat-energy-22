2619a027ba557e473005abde516e8f4c7bb24f3b
Olha <kornia.olha@gmail.com>
2021-05-07 17:41:37 +0300

    Доделала детали и блок с котами

---
+++ source/js/page-header.js
@@ -0,0 +1,12 @@
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('page-header--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('page-header--opened')) {
+    navMain.classList.remove('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--opened');
+  }
+});
