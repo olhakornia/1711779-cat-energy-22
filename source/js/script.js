f1f079e9c175ded1c6b0a29c84b0b101eda2422f
Olha <kornia.olha@gmail.com>
2021-05-13 15:25:22 +0300

    fixed

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var navMain = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('page-header--nojs');
+navMain.classList.remove('page-header--opened');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('page-header--opened')) {
